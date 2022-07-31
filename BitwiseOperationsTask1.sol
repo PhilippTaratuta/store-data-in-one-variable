45bec325f02770e42268207c95ff926b12e02bae
philipptaratuta <philipp.t@blaize.tech>
2022-07-31 18:44:39 +0300

    Initial task

---
+++ BitwiseOperationsTask1.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.15;
+
+contract BitwiseOperationsTask1 {
+
+    uint256 public data;
+
+    function setData(address value1, uint64 value2, uint32 value3) external {
+        // Write code here.
+    }
+
+    function getAddress() external view returns (address result) {
+        // Write code here.
+    }
+
+    function getUint64() external view returns (uint64 result) {
+        // Write code here.
+    }
+
+    function getUint32() external view returns (uint32 result) {
+        // Write code here.
+    }
+}
